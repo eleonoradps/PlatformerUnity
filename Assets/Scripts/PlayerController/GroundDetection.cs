c459e640e5a19ae1c2c8cbc396afcb0d3d156870
L <eleonora.dups@gmail.com>
2019-11-15 18:54:25 +0100

    modified jump, add friction and ground check

---
+++ Assets/Scripts/PlayerController/GroundDetection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundDetection : MonoBehaviour
+{
+    [SerializeField] private LayerMask platformLayerMask;
+    [SerializeField] public bool isGrounded;
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        isGrounded = collider != null;
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        isGrounded = false;
+    }
+}
--- Assets/Scripts/PlayerController/Player.cs
+++ Assets/Scripts/PlayerController/Player.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float fallMultiplier = 2.5f; // multiply gravity
+    [SerializeField] private float lowJumpMultiplier = 2f; // increase gravity after jump
     [SerializeField] private Rigidbody2D body;
 
     [SerializeField] Vector2 direction;
 
     [SerializeField] private float speed = 4;
-    
+    [SerializeField] GroundDetection groundDetection;
+
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
@@ -20,16 +23,34 @@ public class Player : MonoBehaviour
         body.velocity = new Vector2(direction.x * speed, body.velocity.y);
     }
 
-    
+
     void Update()
     {
         direction = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
         Debug.Log(body.velocity.x);
-        
-        if(Input.GetKeyDown("space"))
+
+        if (Input.GetKeyDown("space") && IsGrounded())
         {
-            body.velocity = new Vector2(1, 5);
+            body.velocity = new Vector2(body.velocity.x, 5);
+            Debug.Log("ICI");
         }
 
+        //if (body.velocity.y < 0) // if body's velocity y is less than 0, apply fallMultiplier to gravity
+        //{
+        //    body.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        //    // minus 1 bc multiply fallMultiplier which is 2.5
+        //    // delta time bc gravity is per second and we want this to be a fraction of a second
+        //}
+        //// else if we're jumping up and !GetButton apply the same thing but instead it's lowJump
+        //else if (body.velocity.y > 0 && !Input.GetButton("space"))
+        //{
+        //    body.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        //}
+
+    }
+
+    private bool IsGrounded()
+    {
+        return groundDetection.isGrounded;
     }
 }
